95f13693735509a2873b0bb13ce51f5ce7005c10
Luizzzin <luizhenribarbodias@gmail.com>
2025-04-05 15:45:53 -0300

    feat: classe academia

--- src/models/Academia.java
+++ src/models/Academia.java
@@ -1,4 +1,27 @@
 package models;
+import java.util.ArrayList;
+//composicao e polimorfismo
 
 public class Academia {
+
+    private String nomeAcademia;
+    private ArrayList<Pessoa> pessoasAcademia;
+
+
+    public Academia(String nomeAcademia) {
+        this.nomeAcademia = nomeAcademia;
+        this.pessoasAcademia = new  ArrayList<>();
+    }
+
+    public void adicionarPessoa(Pessoa pessoa){
+        pessoasAcademia.add(pessoa);
+    }
+    public void listarPessoas(){
+        System.out.println("Listas de pessoas: ");
+        for (Pessoa pessoa : pessoasAcademia){
+            System.out.println(pessoa.getDescricao());
+        }
+
+    }
+
 }
